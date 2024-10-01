d39d07193c3793ee6a5f528862483549b4fcdc58
eunground <jeshin@sookmyung.ac.kr>
2024-10-01 22:45:11 +0900

    Feat: step1 리팩터링 및 회고

--- 신정은/2주차 실습/index.js
+++ 신정은/2주차 실습/index.js
@@ -1,9 +1,3 @@
-// 오늘 얻은 인사이트
-// 1. 이벤트 위임을 어떻게 할 수 있는지 알게 됨
-// 2. 요구 사항을 전략적으로 접근해야하는지, 단계별로 세세하게 나누는 것이 중요
-// 3. DOM 요소를 가져올 때는 $표시를 써서 변수처럼 사용할 수 있다
-// 4. 새롭게 알게 된 메서드 innerText, innerHTML, insertAdjacentHtml, closest, e.target
-
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
 // - [x] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
@@ -13,46 +7,20 @@
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
-// TODO 메뉴 수정
-// - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
-// - [x] 모달창에서 신규 메뉴명을 입력 받고, 확인 버튼을 누르면 메뉴가 수정된다.
-
-// TODO 메뉴 삭제
-// - [x] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
-
 // 달러표시는 js에서 DOM 엘리먼트, HTML DOM 엘리먼트를 가져올 때 관용적으로 사용한다.
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+    // form 태그가 자동으로 전송되는 것을 막아줍니다.
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
+        e.preventDefault();
+    });
+
     // 메뉴 개수를 업데이트하는 함수
     const updateMenuCount = () => {
         const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
         $(".menu-count").innerText = `총 ${menuCount}개`;
     };
-
-    $("#espresso-menu-list").addEventListener("click", (e) => {
-        if (e.target.classList.contains("menu-edit-button")) {
-            const menuName = e.target.closest("li").querySelector(".menu-name").innerText;
-            const updateMenuName = prompt("메뉴명을 수정하세요", menuName);
-            if (updateMenuName) {
-                e.target.closest("li").querySelector(".menu-name").innerText = updateMenuName;
-            }
-        }
-
-        if (e.target.classList.contains("menu-remove-button")) {
-            if (confirm("정말 삭제하시겠습니까?")) {
-                e.target.closest("li").remove();
-                updateMenuCount();
-            }
-        }
-    });
-
-    // form 태그가 자동으로 전송되는 것을 막아줍니다.
-    $("#espresso-menu-form").addEventListener("submit", (e) => {
-        e.preventDefault();
-    });
 
     // 메뉴를 추가하는 함수
     const addMenuName = () => {
