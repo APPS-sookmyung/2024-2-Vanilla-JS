eb6d8642e1c1310af70dbec7a05eb41f5ff980fa
misung.dev <misung.dev@gmail.com>
2024-10-03 13:55:33 +0900

    Feat: 상태 & 로컬 스토리지 개념에 대한 소개

--- 류미성/3주차 실습/src/js/index.js
+++ 류미성/3주차 실습/src/js/index.js
@@ -1,32 +1,47 @@
-// 오늘 얻은 인사이트
-// 1. 이벤트 위임을 어떻게할 수 있는지 알게 됨
-// 2. 요구사항을 전략적으로 접근해야하는지, 단계별로 세세하게 나는 것의 중요성을 알게됨
-// 3. DOM 요소를 가져올때는 $를 사용해서 변수처럼 사용할 수 있음
-// 4. 새로 알게된 메서드 - innerText, insertAdjacentHtml, closest, e.target
+//TODO localStorage Read & Write
+// - [] localStorage에 데이터를 저장한다.
+// - [x] 메뉴를 추가할 때 저장
+// - [] 메뉴를 수정할 때 저장
+// - [] 메뉴를 삭제할 때 저장
+// - [] localStorage에 있는 데이터를 읽어온다.
 
-// step1 요구사항 구현을 위한 전략
+//TODO 카테고리별 메뉴판 관리
+// - [] 에스프레소 메뉴판 관리
+// - [] 프라푸치노 메뉴판 관리
+// - [] 블렌디드 메뉴판 관리
+// - [] 티바나 메뉴판 관리
+// - [] 디저트 메뉴판을 관리
 
-// TODO 메뉴 추가
-// - [x] 메뉴의 이름을 입력받고 확인버튼을 누르면 메뉴가 추가된다.
-// - [x] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
-// - [x] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
-// - [x] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
-// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// TODO 페이지 접근시 최초 데이터 Read & Rendering
+// - [] 페이지에 최초로 로딩될 때 localStorage에 에스프레소 메뉴를 읽어온다.
+// - [] 에스프레소 메뉴를 페이지에 그려준다.
 
-// TODO 메뉴 수정
-// - [x] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴수정하는 모달창이 뜬다.
-// - [] 모달창에서 신규 메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
-
-// TODO 메뉴 삭제
-// - [x] 메뉴 삭제버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [x] 확인 버튼을 클릭하면 메뉴가 삭제된다.
-// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// TODO 품절 상태 관리
+// - [] 품절 상태인 경우를 보여줄 수 있게, 풀절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
+// - [] 품절 버튼을 추가한다
+// - [] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
+// - [] 클릭이벤트에서 가장 가까운 li태그의 class 속성 값에 sold-out을 추가한다.
 
 const $ = (selector) => document.querySelector(selector);
 
+// 로컬스토리지에는 문자열 형태로만 저장하기. object 형태로 그대로 저장해서 관리할 수 없음.
+// JSON 객체 형태를 문자열로 저장하기 위해, 사용하는 메서드가 JSON.stringify()
+// 해당 JSON 객체를 문자열로 저장가능
+const store = {
+	setLocalStorage(menu) {
+		// localStorage.setItem("menu", menu)
+		localStorage.setItem("menu", JSON.stringify(menu));
+	},
+
+	getLocalStorage() {
+		localStorage.getItem("menu");
+	},
+};
+
 function App() {
+	// 상태는 변할 수 있는 데이터 (메뉴명)
+	// 갯수는 배열로 있으면 자동으로 구할 수 있기 때문에, 로컬스토리지에서 굳이 관리할 필요는 없음 (관리: 저장하고 읽어오기)
+	// 데이터는 우리가 꼭 관리해야하는 것만 관리해야함. 그렇지 않으면 코드가 복잡해짐
 	const updateMenuCount = () => {
 		const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
 		$(".menu-count").innerText = `총 ${menuCount} 개`;
