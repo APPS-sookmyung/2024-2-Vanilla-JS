7445cb09fb3a2e945614cde6a9fc274f48dc56dd
misung.dev <misung.dev@gmail.com>
2024-11-06 01:14:32 +0900

    Feat: 34. MenuApi 객체 분리 - 메뉴 생성 리팩터링

--- 류미성/3주차 실습/src/js/index.js
+++ 류미성/3주차 실습/src/js/index.js
@@ -2,7 +2,7 @@ import { $ } from "./utils/dom.js";
 import store from "./store/index.js";
 // TODO 서버 요청 부분
 // - [x] 웹 서버를 띄운다.
-// - [] 서버에 새로운 메뉴명이 추가될 수 있도록 요청한다.
+// - [x] 서버에 새로운 메뉴명이 추가될 수 있도록 요청한다.
 // - [] 서버에 카테고리별 메뉴리스트를 불러온다.
 // - [] 서버에 메뉴가 수정 될 수 있도록 요청한다.
 // - [] 서버에 메뉴의 품절상태를 토글될 수 있도록 요청한다.
@@ -23,6 +23,19 @@ const MenuApi = {
   async getAllMenuByCategory(category) {
     const response = await fetch(`${BASE_URL}/category/${category}/menu`);
     return response.json();
+  },
+
+  async createMenu(category, name) {
+    const response = await fetch(`${BASE_URL}/category/${category}/menu`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+    if (!response.ok) {
+      console.error("에러발생");
+    }
   },
 };
 
@@ -92,17 +105,7 @@ function App() {
     }
 
     const menuName = $("#menu-name").value;
-
-    await fetch(`${BASE_URL}/category/${this.currentCategory}/menu`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ name: menuName }),
-    }).then((response) => {
-      return response.json();
-    });
-
+    await MenuApi.createMenu(this.currentCategory, menuName);
     this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
       this.currentCategory
     );
@@ -173,14 +176,16 @@ function App() {
       addMenuName();
     });
 
-    $("nav").addEventListener("click", (e) => {
+    $("nav").addEventListener("click", async (e) => {
       const isCategoryButton =
         e.target.classList.contains("cafe-category-name");
-      // 예외처리
       if (isCategoryButton) {
         const categoryName = e.target.dataset.categoryName;
         this.currentCategory = categoryName;
         $("#category-title").innerText = `${e.target.innerText} 메뉴 관리`;
+        this.menu[this.currentCategory] = await MenuApi.getAllMenuByCategory(
+          this.currentCategory
+        );
         render();
       }
     });
